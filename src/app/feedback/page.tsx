d6af8730992c2cc5eeddd95799596293911bd4cb
euherrera <eu.herrera@gmail.com>
2025-06-18 17:42:55 +0100

    remote url

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -20,7 +20,7 @@ export default function Feedback() {
         const { name, email, message } = data
 
         // Send data to API route
-        const res = await fetch('https://euherrera.github.io/NextJS---form/api/feedback', {
+        const res = await fetch('/api/feedback', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
