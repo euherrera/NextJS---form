1585f00bed947e02f00975d9cfab0f8cf8711229
euherrera <eu.herrera@gmail.com>
2025-06-18 17:22:16 +0100

    mod files

--- src/app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-// export async function GET() {
-//   return new Response('Hello, Next.js!')
-// }
-
-export async function GET() {
-  return NextResponse.json({'message': 'Helloo, Next.js!'})
-}
-//static route
